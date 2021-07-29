c09ebad475b39d8ff9ba3232acde9e2490a29590
Maja97 <majaos137@gmail.com>
2021-07-29 21:55:41 +0200

    Server

--- be/server/server.js
+++ be/server/server.js
@@ -9,7 +9,4 @@ routesIndex(app);
 app.use(cors());
 app.use(express.static("build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "build", "index.html"));
-});
 app.listen(process.env.PORT || 80);
