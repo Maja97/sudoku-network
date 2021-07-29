3b67dbb927794baf0a1f58d00422eddb1331bc9d
Maja97 <majaos137@gmail.com>
2021-07-29 20:21:54 +0200

    cors

--- be/server/server.js
+++ be/server/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import routesIndex from "./routes/index.js";
+import cors from "cors";
 
 const app = express();
 
 routesIndex(app);
+app.use(cors());
 app.use("/", express.static(path.join(__dirname, "dist")));
 app.listen(process.env.PORT || 80);
