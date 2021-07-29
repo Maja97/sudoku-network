58c0651a763bd689f44ee4ff285477b04a1e8ed1
Maja97 <majaos137@gmail.com>
2021-07-29 19:52:45 +0200

    Port

--- be/server/server.js
+++ be/server/server.js
@@ -4,5 +4,5 @@ import routesIndex from "./routes/index.js";
 const app = express();
 
 routesIndex(app);
-
+app.use("/", express.static(path.join(__dirname, "dist")));
 app.listen(process.env.PORT || 80);
