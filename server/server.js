3c03ae3ad9e6e8deb3e23414e1f5cb8d51bf5aa0
Maja97 <majaos137@gmail.com>
2021-07-30 17:21:52 +0200

    changes

--- server/server.js
+++ server/server.js
@@ -9,10 +9,10 @@ const __dirname = path.resolve();
 routesIndex(app);
 app.use(cors());
 
-app.use(express.static(path.join("fe", "build"), { root: ".." }));
+app.use(express.static(path.join(__dirname, "fe", "build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join("fe", "build", "index.html"), { root: ".." });
+  res.sendFile(path.join(__dirname, "fe", "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 3001);
