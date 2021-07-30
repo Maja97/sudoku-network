c46f5739114c77d1f6a962a26e914cb3c215ac95
Maja97 <majaos137@gmail.com>
2021-07-30 15:48:12 +0200

    backend

--- be/server/server.js
+++ be/server/server.js
@@ -6,6 +6,12 @@ const app = express();
 
 routesIndex(app);
 app.use(cors());
-app.use(express.static("build"));
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "fe", "build", "index.tsx"));
+});
 
 app.listen(process.env.PORT || 3001);
