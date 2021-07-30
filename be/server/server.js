c98241649c90f5f174933d583052e62c36daa096
Maja97 <majaos137@gmail.com>
2021-07-30 16:53:48 +0200

    backend

--- be/server/server.js
+++ be/server/server.js
@@ -9,7 +9,7 @@ const __dirname = path.resolve();
 routesIndex(app);
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join("fe", "build"), { root: ".." }));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join("fe", "build", "index.html"), { root: ".." });
